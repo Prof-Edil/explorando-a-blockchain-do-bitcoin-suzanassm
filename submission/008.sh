d879e97fbd6c7b4919275cd6180501fcba11ce85
Suzana Miranda <136357468+suzanassm@users.noreply.github.com>
2025-01-30 20:05:38 -0300

    Update 008.sh

--- submission/008.sh
+++ submission/008.sh
@@ -3,10 +3,13 @@
 RPC_CONNECT="84.247.182.145"  # IP do servidor RPC
 RPC_USER="user_261"
 RPC_PASSWORD="Uu6LBPhhk9Fr"
- # Sua senha RPC
 #!/bin/bash
-# Get the transaction details.
-tx=$(bitcoin-cli  -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD  getrawtransaction "e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163" true)
-# Extract the public key from input 0.
-pubkey=$(echo $tx | jq -r '.vin[0].scriptSig.asm | split(" ")[1]')
+
+
+tx=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD getrawtransaction "e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163" true)
+
+witness=$(echo $tx | jq -r '.vin[0].txinwitness[2]')
+
+pubkey=$(echo $witness | cut -c 5-70)
+
 echo $pubkey

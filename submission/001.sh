270c8f18d2dc3b6086cadd8d4704b405178bed7b
Suzana Miranda <136357468+suzanassm@users.noreply.github.com>
2025-01-30 19:20:04 -0300

    Update 001.sh

--- submission/001.sh
+++ submission/001.sh
@@ -1,5 +1,5 @@
 # What is the hash of block 654,321?
-RPC_CONNECT="84.247.182.145"
-RPC_USER="user_261"
-RPC_PASSWORD="Uu6LBPhhk9Fr"
-bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD  getblockhash 654321
+#!/bin/bash
+# Get the hash of block 654,321 using the getblockhash RPC command.
+blockhash=$(bitcoin-cli getblockhash 654321)
+echo $blockhash

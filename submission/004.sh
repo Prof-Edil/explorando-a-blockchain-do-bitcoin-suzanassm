b31ba5876164c41bfefe71c6280c027e620aa9b3
Suzana Miranda <136357468+suzanassm@users.noreply.github.com>
2025-01-31 10:56:35 -0300

    Update 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -1,13 +1,21 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
+
+
 RPC_CONNECT="84.247.182.145"
 RPC_USER="user_261"
 RPC_PASSWORD="Uu6LBPhhk9Fr"
 XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
-
 #!/bin/bash
 
-descriptor="tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/0/100)"
-descriptor_with_checksum=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD getdescriptorinfo "$descriptor" | jq -r .descriptor)
-address=$(bitcoin-cli deriveaddresses "$descriptor_with_checksum")
+
+
+xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
+
+descriptor="tr([00000000/100h]$xpub/0/*)"
+
+checked_descriptor=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD getdescriptorinfo "$descriptor" | jq -r '.descriptor')
+
+address=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD deriveaddresses "$checked_descriptor" "[0,0]" | jq -r '.[0]')
+
 echo $address

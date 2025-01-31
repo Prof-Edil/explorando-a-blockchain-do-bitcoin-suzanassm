617fbd9798f73414b29ed802ef614e72ffad16cf
Suzana Miranda <136357468+suzanassm@users.noreply.github.com>
2025-01-31 13:57:40 -0300

    Update 007.sh

--- submission/007.sh
+++ submission/007.sh
@@ -1,22 +1,28 @@
 # Only one single output remains unspent from block 123,321. What address was it sent to?
+# Only one single output remains unspent from block 123,321. What address was it sent to?
 RPC_CONNECT="84.247.182.145"
 RPC_USER="user_261"
 RPC_PASSWORD="Uu6LBPhhk9Fr"
-BLOCK_HEIGHT=123321        # Altura do bloco desejado
 
-#!/bin/bash
-# Get the block hash for block 123,321.
-blockhash=$(bitcoin-cli  -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD getblockhash 123321)
-# Get the block details.
-block=$(bitcoin-cli  -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD  getblock $blockhash 2)
-# Get all transaction IDs in the block.
-txids=$(echo $block | jq -r '.tx[] | .txid')
-# Loop through each transaction to find unspent outputs.
-for txid in $txids; do
-    tx=$(bitcoin-cli   -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD getrawtransaction $txid true)
-    vouts=$(echo $tx | jq -r '.vout[] | select(.spent == false) | .scriptPubKey.address')
-    if [ ! -z "$vouts" ]; then
-        echo $vouts
-        break
+blockhash=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD getblockhash 123321)
+
+block=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD getblock $blockhash)
+
+txids=$(echo "$block" | jq -r '.tx[]')
+
+echo "$txids" | while read -r txid; do
+
+  tx=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD getrawtransaction "$txid" true)
+
+  echo "$tx" | jq -c '.vout[]' | while read -r vout; do
+    index=$(echo "$vout" | jq -r '.n')  # Get the output index
+    address=$(echo "$vout" | jq -r '.scriptPubKey.address')
+
+    utxo=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD gettxout "$txid" "$index")
+
+    if [ ! -z "$utxo" ] && [ "$address" != "null" ]; then
+      echo "$address" 
     fi
+  done
 done
+

38ae9f9a635c0dc9041eeae50064eb09a7c56427
Suzana Miranda <136357468+suzanassm@users.noreply.github.com>
2025-02-02 09:52:14 -0300

    Update 005.sh

--- submission/005.sh
+++ submission/005.sh
@@ -5,8 +5,40 @@ RPC_USER="user_261"
 RPC_PASSWORD="Uu6LBPhhk9Fr"
 #!/bin/bash
 
-xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
-descriptor="tr($xpub/100)"
-checked_descriptor=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD getdescriptorinfo "$descriptor" | jq -r '.descriptor')
-address=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD deriveaddresses "$checked_descriptor" | jq -r '.[0]')
-echo "$address"
+txid="37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517"
+
+raw_tx=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD getrawtransaction "$txid")
+
+decoded_tx=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD decoderawtransaction "$raw_tx")
+
+pubkeys=()
+for input in $(echo "$decoded_tx" | jq -c '.vin[]'); do
+    txid_input=$(echo "$input" | jq -r '.txid')
+    vout=$(echo "$input" | jq -r '.vout')
+
+    prev_raw_tx=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD getrawtransaction "$txid_input")
+    prev_decoded_tx=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD decoderawtransaction "$prev_raw_tx")
+
+    scriptPubKey=$(echo "$prev_decoded_tx" | jq -r ".vout[$vout].scriptPubKey.asm")
+
+    witness=$(echo "$input" | jq -r '.txinwitness')
+    if [ "$witness" != "null" ]; then
+        pubkey=$(echo "$witness" | jq -r '.[1]')  # Extract second witness element (public key)
+        pubkeys+=("$pubkey")
+    fi
+done
+
+echo "Public Keys:"
+for pubkey in "${pubkeys[@]}"; do
+    echo "$pubkey"
+done
+
+pubkey_json=$(printf '"%s",' "${pubkeys[@]}" | sed 's/,$//')
+
+result=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD createmultisig 1 "[$pubkey_json]" "p2sh-segwit")
+
+address=$(echo "$result" | jq -r '.address')
+
+echo  $address
+
+

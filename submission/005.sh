78a6a8399a1815a42ac7234f2ca8b985581d6a92
Suzana Miranda <136357468+suzanassm@users.noreply.github.com>
2025-02-02 12:07:47 -0300

    Update 005.sh

--- submission/005.sh
+++ submission/005.sh
@@ -8,33 +8,24 @@ RPC_PASSWORD="Uu6LBPhhk9Fr"
 txid="37d966a263350fe747f1c606b159987545844a493dd38d84b070027a895c4517"
 
 raw_tx=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD getrawtransaction "$txid")
-
 decoded_tx=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD decoderawtransaction "$raw_tx")
 
 pubkeys=()
 for input in $(echo "$decoded_tx" | jq -c '.vin[]'); do
     txid_input=$(echo "$input" | jq -r '.txid')
     vout=$(echo "$input" | jq -r '.vout')
-
     prev_raw_tx=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD getrawtransaction "$txid_input")
     prev_decoded_tx=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD decoderawtransaction "$prev_raw_tx")
-
     scriptPubKey=$(echo "$prev_decoded_tx" | jq -r ".vout[$vout].scriptPubKey.asm")
-
     witness=$(echo "$input" | jq -r '.txinwitness')
     if [ "$witness" != "null" ]; then
-        pubkey=$(echo "$witness" | jq -r '.[1]')  # Extract second witness element (public key)
+        pubkey=$(echo "$witness" | jq -r '.[1]')
         pubkeys+=("$pubkey")
     fi
 done
 
-
 pubkey_json=$(printf '"%s",' "${pubkeys[@]}" | sed 's/,$//')
-
-result=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD createmultisig 1 "[$pubkey_json]" "p2sh-segwit")
-
+result=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD createmultisig 1 "[$pubkey_json]" "legacy")
 address=$(echo "$result" | jq -r '.address')
 
-echo  $address
-
-
+echo $address

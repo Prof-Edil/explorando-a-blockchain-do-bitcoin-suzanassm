b629625cfcf4e4c67654fedbf6d3efddf2b92c99
Suzana Miranda <136357468+suzanassm@users.noreply.github.com>
2025-02-02 09:38:58 -0300

    Update 004.sh

--- submission/004.sh
+++ submission/004.sh
@@ -8,14 +8,8 @@ RPC_PASSWORD="Uu6LBPhhk9Fr"
 XPUB="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
 #!/bin/bash
 
-
-
 xpub="xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2"
-
-descriptor="tr([00000000/100h]$xpub/0/*)"
-
+descriptor="tr($xpub/100)"
 checked_descriptor=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD getdescriptorinfo "$descriptor" | jq -r '.descriptor')
-
-address=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD deriveaddresses "$checked_descriptor" "[0,0]" | jq -r '.[0]')
-
+address=$(bitcoin-cli -rpcconnect=$RPC_CONNECT -rpcuser=$RPC_USER -rpcpassword=$RPC_PASSWORD deriveaddresses "$checked_descriptor" | jq -r '.[0]')
 echo $address
